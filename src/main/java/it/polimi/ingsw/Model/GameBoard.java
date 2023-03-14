6049187ca9b446214d0722ee0ae8f2afcddb476e
peetceenatoo <piccinatomattia@gmail.com>
2023-03-14 13:45:13 +0100

    Modified GameBoard class

--- src/main/java/it/polimi/ingsw/Model/GameBoard.java
+++ src/main/java/it/polimi/ingsw/Model/GameBoard.java
@@ -1,20 +1,48 @@
 package it.polimi.ingsw.Model;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
 public class GameBoard {
     private Map<Coordinates, Optional<Tile>> board;
     private int tilesOnBoard;
-    private GameBoard(){}
-    public static GameBoard gameBoardTwo(){}
-    public static GameBoard gameBoardThree(){}
-    public static GameBoard gameBoardFour(){}
+    private GameBoard(){
 
-    private void makeItTwo(){}
-    private void makeItThree(){}
-    private void makeItFour(){}
-    public int getNumberOfTiles(){}
-    public Set<Coordinates> getCoords(){}
-    public Optional<Optional<Tile>>  getTile(Coordinates c){}
-    public void setTile(Coordinates c, Tile t){}
-    public Optional<Tile> pickTile(Coordinates c){}
+    }
+    public static GameBoard gameBoardTwo(){
+
+    }
+    public static GameBoard gameBoardThree(){
+
+    }
+    public static GameBoard gameBoardFour(){
+
+    }
+
+    private void makeItTwo(){
+
+    }
+    private void makeItThree(){
+
+    }
+    private void makeItFour(){
+
+    }
+    public int getNumberOfTiles(){
+
+    }
+    public Set<Coordinates> getCoords(){
+
+    }
+    public Optional<Tile>  getTile(Coordinates c){
+
+    }
+    public void setTile(Coordinates c, Tile t){
+
+    }
+    public Optional<Tile> pickTile(Coordinates c){
+
+    }
 
 }

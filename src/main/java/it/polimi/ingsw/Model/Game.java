da899c8a12b23943cb15c75b4000055da24512b2
peetceenatoo <piccinatomattia@gmail.com>
2023-03-14 13:30:22 +0100

    Modified Game class

--- src/main/java/it/polimi/ingsw/Model/Game.java
+++ src/main/java/it/polimi/ingsw/Model/Game.java
@@ -1,4 +1,63 @@
 package it.polimi.ingsw.Model;
 
+import java.util.Optional;
+import java.util.Set;
+
 public class Game {
+    private GameBoard board;
+    private Player[] players;
+    private PublicGoal[] goals;
+    private int currentPlayer;
+    private TilesSack sack;
+
+    public Game() {
+
+    }
+
+    public int getCurrentPlayer(){
+
+    }
+
+    public void setCurrentPlayer(){
+
+    }
+
+    public Player getPlayer(int p){
+
+    }
+
+    public void addPlayer(Player p){
+
+    }
+
+    public void checkPublicGoals(){
+
+    }
+
+    public Tile popFromSack(){
+
+    }
+
+    public Set<Coordinates> getNumberOfTilesFromBoard(){
+
+    }
+
+    public Optional<Tile> getTileFromBoard(Coordinates c){
+
+    }
+
+    public void setTileFromBoard(Coordinates c, Tile t){
+
+    }
+
+    public Optional<Tile> pickTileFromBoard(Coordinates c){
+
+    }
+
+    private void pickTwoPublicGoals(){
+
+    }
+
+
+
 }

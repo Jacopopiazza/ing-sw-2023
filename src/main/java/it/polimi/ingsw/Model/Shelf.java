7dec0fb5a4244dd2bfcabb4353c53734d0edde0c
peetceenatoo <piccinatomattia@gmail.com>
2023-03-14 14:06:07 +0100

    Modified Shelf class

--- src/main/java/it/polimi/ingsw/Model/Shelf.java
+++ src/main/java/it/polimi/ingsw/Model/Shelf.java
@@ -1,4 +1,29 @@
 package it.polimi.ingsw.Model;
 
+import java.util.Optional;
+
 public class Shelf {
+    private final Optional<Tile>[][];
+    private final int columns;
+    private final int rows;
+
+    public void addTile(Tile tile, int column){
+
+    }
+
+    public Optional<Tile> getTile(Coordinates c){
+
+    }
+
+    public int getColumns(){
+        return columns;
+    }
+
+    public int getRows(){
+        return rows;
+    }
+
+    public void empty(){
+
+    }
 }

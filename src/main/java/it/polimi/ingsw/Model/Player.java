d2063343544c16518c1020103c15095522c1258a
peetceenatoo <piccinatomattia@gmail.com>
2023-03-14 13:38:01 +0100

    Modified Player class

--- src/main/java/it/polimi/ingsw/Model/Player.java
+++ src/main/java/it/polimi/ingsw/Model/Player.java
@@ -1,4 +1,35 @@
 package it.polimi.ingsw.Model;
 
 public class Player {
+
+    private int score;
+    private Shelf shelf;
+    private PrivateGoal goal;
+
+    public Player(){
+
+    }
+    public void insert(Tile t[], int column){
+
+    }
+
+    public int getScore(){
+
+    }
+
+    public void setScore(){
+
+    }
+
+    //not sure that we need this one
+    public Shelf getShelf(){
+
+    }
+
+    //checks the boardGoal and its privateGoal, may use further private methods(?)
+    public void finalCheck(){
+
+    }
+
+
 }

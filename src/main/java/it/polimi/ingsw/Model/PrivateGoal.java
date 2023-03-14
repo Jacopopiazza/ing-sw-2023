704e0f280f5fc94390c447f95f17ebe104931486
JPiazza <jacopo@ideologica.it>
2023-03-14 13:50:35 +0100

    Created PrivateGoal

--- src/main/java/it/polimi/ingsw/Model/PrivateGoal.java
+++ src/main/java/it/polimi/ingsw/Model/PrivateGoal.java
@@ -1,4 +1,13 @@
 package it.polimi.ingsw.Model;
 
 public class PrivateGoal {
+    private Coordinates[] coords;
+
+    public PrivateGoal(Coordinates[] coords) {
+        this.coords = coords;
+    }
+
+    public int check(Shelf shelf){
+
+    }
 }

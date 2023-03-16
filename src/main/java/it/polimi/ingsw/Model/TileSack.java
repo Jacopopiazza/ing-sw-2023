78c4c0ec60f26b933711e57ea370dc54300feb37
Simone Romanò <simone4.romano@mail.polimi.it>
2023-03-16 20:19:32 +0100

    Implemented the first method of TileSack

--- src/main/java/it/polimi/ingsw/Model/TileSack.java
+++ src/main/java/it/polimi/ingsw/Model/TileSack.java
@@ -1,16 +1,19 @@
 package it.polimi.ingsw.Model;
 
 public class TileSack {
-    private int[] remaining;
-    private final static int LEN = TileColor.values().length;
+    private int[] remaining;    // Index is the corresponding color in TileColor
+    private final static int LEN = TileColor.values().length;   // length of the array
     private final static int NUM_OF_TILES_PER_COLOR = 22;
 
     public TileSack(){
-
+        // initialize the array with the maximum value for every element
+        for(int i = 0; i < LEN; i++)
+            remaining[i] = NUM_OF_TILES_PER_COLOR;
     }
 
     public Tile pop(){
-        // MMEH
+        // Check that returns a Tile object != null
+        //Todo: how to generate the color i want to pop?
     }
 
 }

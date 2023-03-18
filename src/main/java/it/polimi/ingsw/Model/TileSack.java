08820d27fac929e7234b5e3715166445d7ce5b32
Simone Romanò <simone4.romano@mail.polimi.it>
2023-03-18 11:46:48 +0100

    Completed the class TilesSack

--- src/main/java/it/polimi/ingsw/Model/TileSack.java
+++ src/main/java/it/polimi/ingsw/Model/TileSack.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.Model;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class TileSack {
     private int[] remaining;    // Index is the corresponding color in TileColor
     private final static int LEN = TileColor.values().length;   // length of the array
@@ -11,9 +14,25 @@ public class TileSack {
             remaining[i] = NUM_OF_TILES_PER_COLOR;
     }
 
-    public Tile pop(){
-        // Check that returns a Tile object != null
-        //Todo: how to generate the color i want to pop?
+    // Generate a Tile with a random color and pops it from the sack
+    public Tile pop() throws InvalidColorException{
+        int random_color_index = 0;
+        // Generate a number from 0 to 'remaining Tiles' - 1
+        random_color_index = new Random().nextInt(Arrays.stream(remaining).sum());
+
+        for(int i = 0; i < LEN; i++){
+            if(random_color_index >= i * NUM_OF_TILES_PER_COLOR &&
+                    random_color_index < (i + 1) * NUM_OF_TILES_PER_COLOR) {
+                try {
+                    // Pop that color from the tileSack
+                    remaining[i]--;
+                    // Return the corresponding Tile
+                    return new Tile(TileColor.values()[random_color_index]);
+                } catch (InvalidColorException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }
     }
 
 }

c7e368c845a34870635a336e43d0edca06cb12ab
Simone Romanò <simone4.romano@mail.polimi.it>
2023-03-18 12:21:22 +0100

    Completed the class Shelf, addTile implemented using reference

--- src/main/java/it/polimi/ingsw/Model/Shelf.java
+++ src/main/java/it/polimi/ingsw/Model/Shelf.java
@@ -1,28 +1,40 @@
 package it.polimi.ingsw.Model;
 
-public class Shelf {
+public class Shelf implements Cloneable{
     private final Tile[][] SHELF;
     private static final int COLUMNS = 6;
     private static final int ROWS = 5;
 
     public Shelf(){
-
+        // initialize the matrix with null values
+        SHELF = new Tile[ROWS][COLUMNS];
     }
 
-    public void addTile(Tile t, int c){
+    public void addTile(Tile t, int column){
+        // If the first row is already filled with a tile, the column is fully filled
+        if(SHELF[0][column] != null)
+            throw new IllegalColumnInsertionException();
 
+        // start analysing from the bottom
+        int row = ROWS - 1;
+        while(SHELF[row][column] != null && row >= 0){
+            row--;
+        }
+
+        // Todo: Do i have to pass the reference or a copy?
+        SHELF[row][column] = t;
     }
 
     public Tile getTile(Coordinates c){
-
+        return SHELF[c.getX()][c.getY()];
     }
 
     public int getColumns(){
-
+        return COLUMNS;
     }
 
     public int getRows(){
-
+        return ROWS;
     }
 
 }

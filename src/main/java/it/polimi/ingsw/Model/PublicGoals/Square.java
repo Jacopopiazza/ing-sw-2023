f0cff230c79f911c51f16de4efcb8bd6897e01e8
Simone Romanò <simone4.romano@mail.polimi.it>
2023-03-20 15:00:17 +0100

    Completed the class Square

--- src/main/java/it/polimi/ingsw/Model/PublicGoals/Square.java
+++ src/main/java/it/polimi/ingsw/Model/PublicGoals/Square.java
@@ -1,13 +1,44 @@
 package it.polimi.ingsw.Model.PublicGoals;
 
-import it.polimi.ingsw.Model.PublicGoal;
-import it.polimi.ingsw.Model.Shelf;
+import it.polimi.ingsw.Model.*;
 
 public class Square extends PublicGoal {
 
     // 2 separated groups of 2x2 Tiles of the same color
     @Override
     public boolean check(Shelf s) {
-
+        int c = s.getColumns();
+        int r = s.getRows();
+        Coordinates firstSquare=null;
+        TileColor color=null;
+        for(int i = 0; i < r - 1; i++){
+            for(int j = 0; j < c - 1; j++){
+                Coordinates coord = new Coordinates(i,j);
+                Tile temp= s.getTile(coord);
+                if(temp != null && (firstSquare == null ||
+                        (color == temp.getColor()
+                            && (!coord.equals(new Coordinates(firstSquare.getX() + 1, firstSquare.getY())))
+                            && (!coord.equals(new Coordinates(firstSquare.getX() + 1, firstSquare.getY() + 1)))
+                            && (!coord.equals(new Coordinates(firstSquare.getX(), firstSquare.getY() + 1)))
+                        )
+                    )
+                ) {
+                    // then check for the color of all the other tiles
+                    if(s.getTile(new Coordinates(firstSquare.getX() + 1, firstSquare.getY())).getColor().equals(temp.getColor())
+                        && s.getTile(new Coordinates(firstSquare.getX() + 1, firstSquare.getY() + 1)).getColor().equals(temp.getColor())
+                        && s.getTile(new Coordinates(firstSquare.getX(), firstSquare.getY() + 1)).getColor().equals(temp.getColor())
+                    ){
+                        // found the first square, set the variables to find the second square: coordinates and color
+                        if(firstSquare == null){
+                            firstSquare = coord;
+                            color = temp.getColor();
+                        }else{
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 }

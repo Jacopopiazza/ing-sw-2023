a72ce0cbd97ed2911b4b664eaac324c6ea3035a8
peetceenatoo <piccinatomattia@gmail.com>
2023-03-20 14:06:35 +0100

    Coordinates class

--- src/main/java/it/polimi/ingsw/Model/Coordinates.java
+++ src/main/java/it/polimi/ingsw/Model/Coordinates.java
@@ -1,8 +1,13 @@
-package it.polimi.ingsw.Model;
+import java.util.Objects;
 
 public class Coordinates {
     private final int x;
     private final int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     public int getX() {
         return x;
@@ -13,12 +18,17 @@ public class Coordinates {
     }
 
     @Override
-    public int hashCode(){
-
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || this.getClass() != o.getClass())
+            return false;
+        Coordinates temp = (Coordinates) o;
+        return x == temp.x && y == temp.y;
     }
 
     @Override
-    public boolean equals(Object o){
-
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }

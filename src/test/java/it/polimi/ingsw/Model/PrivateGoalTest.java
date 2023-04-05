2e7da508567b12a7b3999a66e689f072320d7264
JPiazza <jacopo@ideologica.it>
2023-04-05 12:44:21 +0200

    Fixed private goal test

--- src/test/java/it/polimi/ingsw/Model/PrivateGoalTest.java
+++ src/test/java/it/polimi/ingsw/Model/PrivateGoalTest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
-import java.util.HashSet;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -49,25 +49,54 @@ public class PrivateGoalTest {
         privateGoals = PrivateGoal.getPrivateGoals(-1);
     }
 
+
+
     @Test
     public void testCheckMaximumPoints() throws InvalidNumberOfPlayersException, MissingShelfException, IllegalColumnInsertionException, NoTileException {
         Shelf shelfToTest = new Shelf();
-        Coordinates coord[] = PrivateGoal.getPrivateGoals(2)[0].getCoordinates();
-        boolean flag;
-
-        // from bottom left, row by row, adds every tile
-        for( int i = 0; i < Shelf.getRows()*Shelf.getColumns(); i++ ){
-            flag = false;
-            // for each Coordinate in the PrivateGoal, if present I add such tile (with color TileColor.values()[j])
-            for( int j = 0; j < coord.length && !flag; j++ ) {
-                if ( ( coord[j].getY() == i % Shelf.getColumns() ) && ( coord[j].getX() == Shelf.getRows() - 1 - (i / Shelf.getRows()) ) ) {
-                    shelfToTest.addTile(new Tile(TileColor.values()[j], 0), coord[j].getY());
-                    flag = true;
-                }
-            }
-            // otherwise adds a blue Tile (doesn't matter)
-            if( !flag ) shelfToTest.addTile(new Tile(TileColor.BLUE, 0), i % Shelf.getColumns());
+        privateGoals = PrivateGoal.getPrivateGoals(2);
+        Coordinates[] coord = privateGoals[0].getCoordinates();
+        Map<Coordinates,TileColor> map = new HashMap<>();
+        for(int i = 0;i<coord.length;i++){
+            map.put(coord[i], TileColor.values()[i]);
         }
+
+        coord = Arrays.stream(coord).sorted((c1,c2) -> {
+            int val = 1;
+            if (c1.equals(c2)) return 0;
+            if(c1.getY() < c2.getY() || c1.getY() == c2.getY() && c1.getX() < c2.getX()) return -1;
+
+            return val;
+
+        }).toArray(Coordinates[]::new);
+
+        int lastRow = -1;
+        int lastColumn = -1;
+
+        for(int numero = 0; numero < coord.length; numero++){
+            Coordinates c = coord[numero];
+            int row = c.getX();
+            int column = c.getY();
+
+            if(lastColumn != column) {
+                lastColumn = column;
+                lastRow = 0;
+            }
+            else{
+                lastRow += 1;
+            }
+            //Add casual tiles of casual color to reach the right row
+            for(int i = lastRow; i < row;i++){
+                shelfToTest.addTile(new Tile(TileColor.BLUE, new Random(120).nextInt()),column);
+            }
+            //Add current tile of the correct color in the correct row
+            shelfToTest.addTile(new Tile(map.get(c), new Random(120).nextInt()), column);
+            lastRow = row;
+
+
+
+        }
+
         assertEquals(privateGoals[0].check(shelfToTest), 12);
 
     }

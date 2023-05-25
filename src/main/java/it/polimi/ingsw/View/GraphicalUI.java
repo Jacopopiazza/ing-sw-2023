ae990a8a852e3f31e1e1f22c3b2efc781feb43bd
FraRita <francescorita2709@gmail.com>
2023-05-25 16:58:10 +0200

    finished the home page in GUI

--- src/main/java/it/polimi/ingsw/View/GraphicalUI.java
+++ src/main/java/it/polimi/ingsw/View/GraphicalUI.java
@@ -30,7 +30,7 @@ public class GraphicalUI extends ClientManager {
 
     private class Frame extends JFrame{
 
-        private JPanel content;
+        private JPanel request;
         private JLabel error;
 
         private Frame(){
@@ -38,48 +38,79 @@ public class GraphicalUI extends ClientManager {
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             setSize(1280, 720); // 16:9 proportion
 
-            //add the background
-            JPanel background = new Background("visual_components/misc/sfondo parquet.jpg");
+            //create the background
+            JPanel background = new Background("visual_components/misc/base_pagina2.jpg");
             background.setLayout(new BorderLayout());
-            add(background);
 
-            //add the content
-            content = new JPanel();
+            //create the content
+            JPanel content = new JPanel();
             content.setOpaque(false);
             content.setLayout(new BoxLayout(content,BoxLayout.PAGE_AXIS));
+            content.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+            //add the title
             Image titleImage = new ImageIcon("visual_components/Publisher material/Title 2000x618px.png").getImage();
             ImageIcon titleIcon = new ImageIcon(titleImage.getScaledInstance(1000,309,Image.SCALE_DEFAULT));
-            content.add(new JLabel(titleIcon));
-            content.add(new JLabel("Do you want to use RMI or Socket?"));
+            JLabel title = new JLabel(titleIcon);
+            title.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+            content.add(title);
+
+            //add the request
+            request = new JPanel();
+            request.setOpaque(false);
+            request.setLayout(new BoxLayout(request,BoxLayout.PAGE_AXIS));
+            askConnection();
+            content.add(request);
+
+            //add the error
+            error = new JLabel();
+            error.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+            error.setForeground(Color.decode("13806189"));
+            content.add(error);
+
+            background.add(content,BorderLayout.CENTER);
+            add(background);
+
+            setVisible(true);
+        }
+
+        private void askConnection(){
+            request.removeAll();
+
+            //set up the question
+            JLabel text = new JLabel("Do you want to use RMI or Socket?");
+            text.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+            text.setFont(text.getFont().deriveFont(20f));
+            text.setForeground(Color.decode("13806189"));
+            request.add(text);
+
+            //set up the options
             JPanel choices = new JPanel();
             choices.setOpaque(false);
             choices.setLayout(new FlowLayout());
             JButton rmi = new JButton("RMI");
+            rmi.setPreferredSize(new Dimension(100,40));
+            rmi.setBackground(Color.decode("13806189"));
             rmi.addActionListener((e) -> {
                 if(e.getSource() instanceof JButton button){
                     if(!connectionChosen(button)){
-                        error.setText("Unable to connect");
+                        error.setText("Unable to connect with RMI");
                     }
                 }
             });
             JButton socket = new JButton("Socket");
+            socket.setPreferredSize(new Dimension(100,40));
+            socket.setBackground(Color.decode("13806189"));
             socket.addActionListener((e) -> {
                 if(e.getSource() instanceof JButton button){
                     if(!connectionChosen(button)){
-                        error.setText("Unable to connect");
+                        error.setText("Unable to connect with Socket");
                     }
                 }
             });
             choices.add(rmi);
             choices.add(socket);
-            content.add(choices);
-            error = new JLabel();
-            content.add(error);
-            content.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-
-            background.add(content,BorderLayout.CENTER);
-
-            setVisible(true);
+            request.add(choices);
         }
 
     }

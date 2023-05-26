217b21cd6c4c8cdf943471c197bb2697f068885b
FraRita <francescorita2709@gmail.com>
2023-05-26 17:44:47 +0200

    added PrivateGoalView

---
+++ src/main/java/it/polimi/ingsw/ModelView/PrivateGoalView.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.ModelView;
+
+import it.polimi.ingsw.Model.Coordinates;
+import it.polimi.ingsw.Model.PrivateGoal;
+
+import java.io.Serializable;
+
+public class PrivateGoalView implements Serializable {
+
+    private final Coordinates[] coords;
+
+    public PrivateGoalView(PrivateGoal goal){
+        coords = goal.getCoordinates();
+    }
+
+    public Coordinates[] getCoordinates(){
+        return coords;
+    }
+}

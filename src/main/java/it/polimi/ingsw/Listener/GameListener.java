e84945f70977c8feb42b3570aac3e01bdf7a8627
JPiazza <jacopo@ideologica.it>
2023-05-29 12:08:56 +0200

    Qualcuno ha detto Listener Interfaces JavaDoc

--- src/main/java/it/polimi/ingsw/Listener/GameListener.java
+++ src/main/java/it/polimi/ingsw/Listener/GameListener.java
@@ -3,6 +3,16 @@ package it.polimi.ingsw.Listener;
 import it.polimi.ingsw.Messages.Message;
 import it.polimi.ingsw.Network.Client;
 
+/**
+ * The GameListener interface represents a listener for game updates.
+ * Classes implementing this interface can register themselves as listeners to receive game updates.
+ */
 public interface GameListener {
+
+    /**
+     * This method is called when a game update occurs.
+     *
+     * @param message The message containing the game update information.
+     */
     void update(Message message);
 }
--- src/main/java/it/polimi/ingsw/Listener/ViewListener.java
+++ src/main/java/it/polimi/ingsw/Listener/ViewListener.java
@@ -3,7 +3,17 @@ package it.polimi.ingsw.Listener;
 import it.polimi.ingsw.Messages.Message;
 import it.polimi.ingsw.Network.Client;
 
+/**
+ * The ViewListener interface represents a listener for view messages.
+ * Classes implementing this interface can register themselves as listeners to receive view messages.
+ */
 public interface ViewListener {
+
+     /**
+      * This method is called when a view message is received.
+      *
+      * @param m The view message to be handled.
+      */
      void handleMessage(Message m);
 
 }

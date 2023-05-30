3535206db0ce02da4486cdce3f1063ce296c5091
JPiazza <jacopo@ideologica.it>
2023-05-30 10:26:35 +0200

    Added even more JavaDoc

--- src/main/java/it/polimi/ingsw/ModelView/PlayerView.java
+++ src/main/java/it/polimi/ingsw/ModelView/PlayerView.java
@@ -5,6 +5,10 @@ import it.polimi.ingsw.Model.PrivateGoal;
 
 import java.io.Serializable;
 
+/**
+ * The PlayerView class represents the view of a player in the model-view architecture.
+ * It provides a simplified representation of the player's state for the client-side view.
+ */
 public class PlayerView implements Serializable {
     private static final long serialVersionUID=1L;
     private final int score;
@@ -13,6 +17,12 @@ public class PlayerView implements Serializable {
     private final String username;
     private final int[] accomplishedGlobalGoals;
     private final boolean winner;
+
+    /**
+     * Constructs a new PlayerView object based on the given Player object.
+     *
+     * @param player the Player object to create the view from
+     */
 
     public PlayerView(Player player){
         this.score = player.getScore();
@@ -23,25 +33,60 @@ public class PlayerView implements Serializable {
         this.winner = player.isWinner();
     }
 
+    /**
+     * Retrieves the score of the player.
+     *
+     * @return the player's score
+     */
     public int getScore() {
         return score;
     }
+
+    /**
+     * Retrieves the view of the player's shelf.
+     *
+     * @return the ShelfView object representing the player's shelf
+     */
 
     public ShelfView getShelf() {
         return shelf;
     }
 
+    /**
+     * Retrieves the view of the player's private goal.
+     *
+     * @return the PrivateGoalView object representing the player's private goal
+     */
+
     public PrivateGoalView getPrivateGoal() {
         return privateGoal;
     }
+
+    /**
+     * Retrieves the username of the player.
+     *
+     * @return the player's username
+     */
 
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Retrieves the array of global goal IDs accomplished by the player.
+     *
+     * @return an array of global goal IDs
+     */
+
     public int[] getAccomplishedGlobalGoals() {
         return accomplishedGlobalGoals;
     }
+
+    /**
+     * Checks if the player is a winner.
+     *
+     * @return true if the player is a winner, false otherwise
+     */
 
     public boolean isWinner() {
         return winner;
--- src/main/java/it/polimi/ingsw/ModelView/TileView.java
+++ src/main/java/it/polimi/ingsw/ModelView/TileView.java
@@ -5,25 +5,52 @@ import it.polimi.ingsw.Model.TileColor;
 
 import java.io.Serializable;
 
+/**
+ * The TileView class represents a view of a tile in the game.
+ * It provides a snapshot of the tile's color and ID in a serializable format.
+ */
 public class TileView implements Serializable {
-
     private static final long serialVersionUID=1L;
-
     private final TileColor COLOR;
     private final int ID;
+
+    /**
+     * Constructs a new TileView object based on the given Tile object.
+     *
+     * @param tile the Tile object to create the view from
+     */
 
     public TileView(Tile tile){
         this.ID = tile.getId();
         this.COLOR = tile.getColor();
     }
 
+    /**
+     * Retrieves the color of the tile.
+     *
+     * @return the color of the tile
+     */
+
     public TileColor getCOLOR() {
         return COLOR;
     }
 
+    /**
+     * Retrieves the ID of the tile.
+     *
+     * @return the ID of the tile
+     */
+
     public int getID() {
         return ID;
     }
+
+    /**
+     * Checks if this TileView is equal to another object.
+     *
+     * @param o the object to compare to
+     * @return true if the objects are equal, false otherwise
+     */
 
     @Override
     public boolean equals(Object o) {

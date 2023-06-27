251a77b1e39c7031a1511ed39d43a18271095fa2
JPiazza <jacopo@ideologica.it>
2023-06-27 19:13:39 +0200

    correctly removed name from connected if player gets kicked from lobby

--- src/main/java/it/polimi/ingsw/Network/ServerImplementation.java
+++ src/main/java/it/polimi/ingsw/Network/ServerImplementation.java
@@ -218,6 +218,9 @@ public class ServerImplementation extends UnicastRemoteObject implements Server 
         logger.log(Level.INFO, "Player " + username + " kicked");
 
         synchronized (playingUsernames) {
+
+            playingUsernames.remove(username);
+
             if( lobby == lobbiesWaitingToStart.peek() && lobby.getNumOfActivePlayers() == 0 ) {
                 lobbiesWaitingToStart.poll();
                 while( lobbiesWaitingToStart.peek() != null && lobbiesWaitingToStart.peek().getNumOfActivePlayers() == 0 ) lobbiesWaitingToStart.poll();

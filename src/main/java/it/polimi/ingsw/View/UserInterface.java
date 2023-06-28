b4c6aa877f5b7fe6c22c0e6b0b3c1f86c68904a2
Mattia <piccinatomattia@gmail.com>
2023-06-28 14:22:34 +0200

    Remove useless methods

--- src/main/java/it/polimi/ingsw/View/UserInterface.java
+++ src/main/java/it/polimi/ingsw/View/UserInterface.java
@@ -43,26 +43,39 @@ public abstract class UserInterface implements Runnable, View
     }
 
     /**
-     * Sets up the socket client by creating a server stub with the specified IP address and port.
-     * Starts a new thread to receive messages from the server.
+     * Adds a ViewListener to the list of listeners. The listener will be notified when events occur.
      *
-     * @throws RemoteException    if there is a remote communication error
-     * @throws NotBoundException  if the server stub is not found in the registry
+     * @param listener the ViewListener to add
      */
-    protected void setUpRMIClient() throws RemoteException, NotBoundException {
-        try{
-            setUpRMIClient("localhost");
-        }catch (RemoteException ex){
-            throw ex;
+    @Override
+    public void addListener(ViewListener listener) {
+        synchronized (listeners) {
+            listeners.add(listener);
         }
-        catch (NotBoundException ex){
-            throw ex;
-        }
-        catch (InvalidIPAddress ex){
-            //Do nothing, impossible to happen
-            throw new RuntimeException("Impossible to happen, but it somehow happened");
-        }
+    }
 
+    /**
+     * Clears the list of listeners, removing all registered listeners.
+     */
+    public void clearListeners(){
+        synchronized (listeners) {
+            listeners.clear();
+        }
+    }
+
+    /**
+     * Notifies all the registered listeners with the specified Message. The listeners will handle the message accordingly.
+     *
+     * @param m the Message to notify the listeners with
+     */
+    @Override
+    public void notifyListeners(Message m) {
+        ClientImplementation.logger.log(Level.INFO,"Notifying UserInterface listeners");
+        synchronized (listeners) {
+            for (ViewListener listener : listeners) {
+                listener.handleMessage(m);
+            }
+        }
     }
 
     /**
@@ -94,26 +107,6 @@ public abstract class UserInterface implements Runnable, View
 
     }
 
-
-    protected void setUpSocketClient() throws RemoteException, NotBoundException {
-
-
-        try{
-            setUpSocketClient("localhost");
-        }catch (RemoteException ex){
-            throw ex;
-        }
-        catch (NotBoundException ex){
-            throw ex;
-        }
-        catch (InvalidIPAddress ex){
-            //Do nothing, impossible to happen
-            throw new RuntimeException("Impossible to happen, but it somehow happened");
-        }
-
-
-    }
-
     protected void setUpSocketClient(String ip) throws RemoteException, NotBoundException, InvalidIPAddress{
 
         if(ip == null || ip.isEmpty() || (!IPAddressValidator.isValidIPAddress(ip) && !IPAddressValidator.isValidURL(ip))) {
@@ -138,42 +131,6 @@ public abstract class UserInterface implements Runnable, View
         }.start();
 
 
-    }
-
-    /**
-     * Adds a ViewListener to the list of listeners. The listener will be notified when events occur.
-     *
-     * @param listener the ViewListener to add
-     */
-    @Override
-    public void addListener(ViewListener listener) {
-        synchronized (listeners) {
-            listeners.add(listener);
-        }
-    }
-
-    /**
-     * Notifies all the registered listeners with the specified Message. The listeners will handle the message accordingly.
-     *
-     * @param m the Message to notify the listeners with
-     */
-    @Override
-    public void notifyListeners(Message m) {
-        ClientImplementation.logger.log(Level.INFO,"Notifying UserInterface listeners");
-        synchronized (listeners) {
-            for (ViewListener listener : listeners) {
-                listener.handleMessage(m);
-            }
-        }
-    }
-
-    /**
-     * Clears the list of listeners, removing all registered listeners.
-     */
-    public void clearListeners(){
-        synchronized (listeners) {
-            listeners.clear();
-        }
     }
 
     /**

191881d94c4e298b48ef70b6d2a4060659fc36b8
JPiazza <jacopo@ideologica.it>
2023-06-28 19:31:21 +0200

    Completed JavaDoc for Tuple

--- src/main/java/it/polimi/ingsw/Tuple.java
+++ src/main/java/it/polimi/ingsw/Tuple.java
@@ -1,17 +1,36 @@
 package it.polimi.ingsw;
 
+/**
+ * This class is used to create a tuple of two elements
+ */
 public class Tuple <T1, T2>{
     private final T1 first;
     private final T2 second;
 
+    /**
+     * This method returns the first element of the tuple
+     *
+     * @return the first element of the tuple
+     */
     public T1 getFirst() {
         return first;
     }
 
+    /**
+     * This method returns the second element of the tuple
+     *
+     * @return the second element of the tuple
+     */
     public T2 getSecond() {
         return second;
     }
 
+    /**
+     * This method creates a new {@code Tuple} of two elements
+     *
+     * @param first the first element of the tuple
+     * @param second the second element of the tuple
+     */
     public Tuple(T1 first, T2 second) {
         this.first = first;
         this.second = second;
